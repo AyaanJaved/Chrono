6547d3f19613a86a40e5d8130fb4e044f497ee05
Ayaan <mail.ayaanjaved@gmail.com>
2024-05-18 03:24:52 +0530

    Add BottomNavigationBar UI
    The bar is makeshift with chips. Need to add routes and stuff.

--- app/src/main/java/com/ayaanjaved/chrono/MainActivity.kt
+++ app/src/main/java/com/ayaanjaved/chrono/MainActivity.kt
@@ -6,9 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ayaanjaved.chrono.ui.theme.ChronoTheme
@@ -19,7 +28,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChronoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                       NavigationBar {
+                           // This can be optimised by storing a list Items and their icons. The data
+                           // structure could be a simple list.
+
+                           // Can also look into extracting this whole into its own composable
+
+                           var selectedItem by remember { mutableIntStateOf(0) }
+
+                           NavigationBarItem(
+                               selected = selectedItem == 0,
+                               onClick = {selectedItem = 0},
+                               icon = { Icon(imageVector = Icons.Filled.Add, contentDescription = "")})
+
+                           NavigationBarItem(
+                               selected = selectedItem == 1,
+                               onClick = {selectedItem = 1},
+                               icon = { Icon(imageVector = Icons.Filled.Add, contentDescription = "")})
+
+                           NavigationBarItem(
+                               selected = selectedItem == 2,
+                               onClick = {selectedItem = 2},
+                               icon = { Icon(imageVector = Icons.Filled.Add, contentDescription = "")})
+                       }
+                    }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
